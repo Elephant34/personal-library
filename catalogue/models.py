364511ab2ef20bcfc5f5523df171db7ef601961e
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-02-28 10:33:00 +0000

    Update models.py

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+ratings_list = (
+        (0,"0"),
+        (1,"1 Star"),
+        (2,"2 Stars"),
+        (3,"3 Stars"),
+        (4,"4 Stars"),
+        (5,"5 Stars!"),
+    )
 
 # Create your models here.
+class Book(models.Model):
+    '''
+    Database to catalogue books
+    '''
+    added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    series = models.CharField(default=None, max_length=256)
+    rating = models.IntegerField(choices=ratings_list)
+    last_read = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Recipe(models.Model):
+    '''
+    Database to catalogue recipies
+    '''
+    added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    source = models.CharField(max_length=256)
+    food_type = models.CharField(default=None, max_length=256)
+    rating = models.IntegerField(choices=ratings_list)
+    last_made = models.DateField()
+
+    def __str__(self):
+        return self.title
