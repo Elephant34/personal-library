6988a23f70911dec56662fb4361305451ea53e91
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-03-01 17:16:42 +0000

    Added links to book and recipe templates
    The templates are just placeholders but oh well

--- catalogue/views.py
+++ catalogue/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'catalogue/home.html', context={})
+
+def books(request):
+    return render(request, 'catalogue/books.html', context={})
+
+def recipes(request):
+    return render(request, 'catalogue/recipes.html', context={})
